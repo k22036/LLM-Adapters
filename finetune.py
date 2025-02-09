e75b980fea334b3bbdedbb520f36fb75a9d0b455
k22036 <131113333+k22036@users.noreply.github.com>
2025-02-09 03:05:56 +0000

    change: without using wandb

--- finetune.py
+++ finetune.py
@@ -292,7 +292,8 @@ def train(
             load_best_model_at_end=True if val_set_size > 0 else False,
             ddp_find_unused_parameters=False if ddp else None,
             group_by_length=group_by_length,
-            report_to="wandb" if use_wandb else None,
+            # report_to="wandb" if use_wandb else None,
+            report_to=None,
             run_name=wandb_run_name if use_wandb else None,
         ),
         data_collator=transformers.DataCollatorForSeq2Seq(
